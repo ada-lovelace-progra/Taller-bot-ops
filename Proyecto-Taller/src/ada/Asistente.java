c766ce98b0011cdd6760535a212319eec85519aa
fedem <fedem@Fede-Net>
2018-05-02 23:48:43 -0300

    que se yo

--- Proyecto-Taller/src/ada/Asistente.java
+++ Proyecto-Taller/src/ada/Asistente.java
@@ -13,7 +13,7 @@ public class Asistente {
 	private int cordialidad;
 	public boolean activo;
 	private boolean cuenta;
-	
+
 	public Asistente() {
 		tabla = new Hashtable<String, ArrayList<String>>();
 		this.activo = this.cuenta = false;
@@ -25,7 +25,7 @@ public class Asistente {
 		if (tabla.containsKey(select))
 			for (String temp : tabla.get(select))
 				if (cod.matches(temp)) {
-					setear_Cordialidad(tabla.get(select), cod);
+					setear_Cordialidad(tabla.get(select).indexOf(cod), tabla.get(select).size());
 					return true;
 				}
 		return false;
@@ -51,7 +51,7 @@ public class Asistente {
 			cargarPeticionesGenerales();
 			for (Entry<String, ArrayList<String>> temp : tabla.entrySet()) {
 				String clave = temp.getKey();
-				if (consulta(clave, cad)) {
+				if (!clave.contains("nombrada") && consulta(clave, cad)) {
 					///// Acciones Especiales
 					if (clave == "despedidas")
 						activo = false;
@@ -61,13 +61,15 @@ public class Asistente {
 					return respuesta(clave);
 				}
 			}
+			if(consulta("nombrada", cad))
+				return respuesta("nombrada");
 			if (cuenta)
 				return "la funcion da: " + new CalculoString().calcularFormat("2+2", "%.3f");
-
 			cuenta = false;
 		}
 		if (consulta("llamadas", cad)) {
 			activo = true;
+			tabla.get("llamadas").clear();
 			return respuesta("llamadas");
 		}
 		return "";
@@ -82,22 +84,35 @@ public class Asistente {
 		}
 	}
 
+	/*
+	 * private int subindice(ArrayList<String> temp) { int ret = 10000, sub =
+	 * cordial(temp); while (ret >= temp.size()) ret = sub; return ret; }
+	 */
 	private int subindice(ArrayList<String> temp) {
-		int ret = 10000, sub = cordial(temp);
-		while (ret >= temp.size())
-			ret = (int) (sub);
-		return ret;
+		return cordial(temp);
 	}
 
 	private int cordial(ArrayList<String> temp) {
-		return (int) (((double) this.cordialidad / tope_cordialidad) * temp.size());
+		int i = (int) (((double) this.cordialidad / tope_cordialidad) * (temp.size() - 1));
+		return i > -1 ? i : 0;
 	}
 
 	private void setear_Cordialidad(ArrayList<String> temp, String cad) {
-		if (this.cordialidad != -1)
+		if (this.cordialidad > 0)
 			this.cordialidad += ((temp.indexOf(cad) * (tope_cordialidad / temp.size())) - this.cordialidad) / 3;
 		else
 			this.cordialidad = temp.indexOf(cad) * (tope_cordialidad / temp.size());
+		if (this.cordialidad > tope_cordialidad)
+			this.cordialidad = tope_cordialidad - 1;
+	}
+
+	private void setear_Cordialidad(int i, int t) {
+		if (t == 1)
+			return;
+		if (this.cordialidad > 0)
+			this.cordialidad = (i * (tope_cordialidad / t) - this.cordialidad);
+		else
+			this.cordialidad = i * (tope_cordialidad / t);
 		if (this.cordialidad > tope_cordialidad)
 			this.cordialidad = tope_cordialidad - 1;
 	}
@@ -108,8 +123,10 @@ public class Asistente {
 			tabla.put(select, (new ArrayList<String>()));
 			entrada = new Scanner(
 					new File((select.contains("respuesta") ? "Respuestas\\" : "Peticiones\\\\") + select + ".dat"));
-			while (entrada.hasNextLine())
-				tabla.get(select).add(entrada.nextLine());
+			while (entrada.hasNextLine()) {
+				String nextLine = entrada.nextLine();
+				tabla.get(select).add(nextLine);
+			}
 			entrada.close();
 		} catch (Exception e) {
 			if (entrada != null)
@@ -117,5 +134,4 @@ public class Asistente {
 			System.out.println("no se encontro el arhivo " + select);
 		}
 	}
-
 }
