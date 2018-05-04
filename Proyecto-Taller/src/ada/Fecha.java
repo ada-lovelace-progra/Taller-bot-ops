42a554986c94257ffef8e44a93af267c82775d87
fedem <fedem@Fede-Net>
2018-05-04 14:29:06 -0300

    tiene tambien lo de comparar fechas

--- Proyecto-Taller/src/ada/Fecha.java
+++ Proyecto-Taller/src/ada/Fecha.java
@@ -2,10 +2,25 @@ package ada;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 public class Fecha {
 	static public String now() {
 		return new SimpleDateFormat ("hh:mm:ss - dd/MM/yyyy").format(new Date());
+	}
+	
+	static public String hasta(int dia, int mes, int ano) {
+		Date actual = new Date();
+		GregorianCalendar FechaDada = new GregorianCalendar(ano,mes,dia);
+		Date hasta= new Date(FechaDada.getTimeInMillis()-actual.getTime());
+		return new SimpleDateFormat ("hh:mm:ss - dd/MM/yyyy").format(hasta);
+	}
+	
+	static public String desde(int dia, int mes, int ano) {
+		Date actual = new Date();
+		GregorianCalendar FechaDada = new GregorianCalendar(ano,mes,dia);
+		Date Desde= new Date(actual.getTime()-FechaDada.getTimeInMillis());
+		return new SimpleDateFormat ("hh:mm:ss - dd/MM/yyyy").format(Desde);
 	}
 }
