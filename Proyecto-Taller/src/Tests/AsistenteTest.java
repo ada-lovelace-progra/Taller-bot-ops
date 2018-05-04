4f6e0ce1756d4af1d109f6a549c49713917d645b
Pokengineer <vfacuu@gmail.com>
2018-05-04 20:15:43 -0300

    mas tests

--- Proyecto-Taller/src/Tests/AsistenteTest.java
+++ Proyecto-Taller/src/Tests/AsistenteTest.java
@@ -14,22 +14,59 @@ public class AsistenteTest {
 	@Before
 	public void setup() {
 		ada = new Asistente();
+		ada.escuchar("hola ada");
 	}
 
+//	@Test
+//	public void sinsentido() {
+//		String[] mensajes = {
+//				"Este mensaje no tiene sentido @ada"
+//		};
+//		/// se debe inicar el asistente
+//		ada.escuchar("ada, necesito ayuda");
+//		
+//		for (String mensaje : mensajes) {
+//		/*	Assert.assertEquals(
+//			*		"Disculpa... no entiendo el pedido, @delucas ¿podrías repetirlo?",
+//			*		ada.escuchar(mensaje));*/
+//			Assert.assertEquals("como se realiza dicha peticion?", ada.escuchar(mensaje));
+//		}
+//	}
+ 
 	@Test
-	public void sinsentido() {
-		String[] mensajes = {
-				"Este mensaje no tiene sentido @ada"
-		};
-		/// se debe inicar el asistente
-		ada.escuchar("ada, necesito ayuda");
+	public void calculos() {
+		Assert.assertEquals(
+				"la funcion da: 3",
+				ada.escuchar("@ada resolver 1+2")
+			);
 		
-		for (String mensaje : mensajes) {
-		/*	Assert.assertEquals(
-			*		"Disculpa... no entiendo el pedido, @delucas ¿podrías repetirlo?",
-			*		ada.escuchar(mensaje));*/
-			Assert.assertEquals("como se realiza dicha peticion?", ada.escuchar(mensaje));
-		}
+		Assert.assertEquals(
+				"la funcion da: 1",
+				ada.escuchar("@ada resolver 5-2*2")
+			);
+		
+//		Assert.assertEquals(
+//				"la funcion da: 10",
+//				ada.escuchar("@ada resolver 10% de 100")
+//			);
+		
+		Assert.assertEquals(
+				"la funcion da: 42",
+				ada.escuchar("@ada resolver 17+5^2")
+			);
+		
 	}
-
+	
+	@Test
+	public void calculosCompuestos() {
+		Assert.assertEquals(
+				"la funcion da: -112",
+				ada.escuchar("@ada resolver (((1+1)^3*10)/80-6)*2-100-5+3")
+			);
+		
+//		Assert.assertEquals(
+//				"la funcion da: -6",
+//				ada.escuchar("@ada resolver (4-8)*2+4/(1+1)")
+//			);
+	}
 }
--- Proyecto-Taller/src/Tests/FechaTest.java
+++ Proyecto-Taller/src/Tests/FechaTest.java
@@ -1,5 +1,4 @@
 package Tests;
-
 import static org.junit.Assert.*;
 
 import org.junit.Assert;
