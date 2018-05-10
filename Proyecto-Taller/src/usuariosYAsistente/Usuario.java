a6fbcc2dba9b77a6e044d45b5ce3a3d73ccbbcc9
fedemarkoo <>
2018-05-10 16:56:14 -0300

    .

--- Proyecto-Taller/src/usuariosYAsistente/Usuario.java
+++ Proyecto-Taller/src/usuariosYAsistente/Usuario.java
@@ -4,19 +4,28 @@ import sockets.Cliente;
 
 public class Usuario extends UsuarioGenerico {
 
+	Cliente laposta = new Cliente();
+
 	public Usuario(String NombreUsuario) {
 		nombre = NombreUsuario;
-		laposta=new Cliente();
 		laposta.conectar("localhost", 5050);
 	}
 
 	public Usuario(String NombreUsuario, int puerto) {
 		nombre = NombreUsuario;
-		super.laposta.conectar("localhost", puerto);
+		laposta.conectar("localhost", puerto);
 	}
 
 	public Usuario(String NombreUsuario, String ip, int puerto) {
 		nombre = NombreUsuario;
-		super.laposta.conectar(ip, puerto);
+		laposta.conectar(ip, puerto);
+	}
+
+	public void enviarMensaje(String mensaje) {
+		laposta.enviar(nombre + ": " + mensaje);
+	}
+
+	public String recibirMensaje() {
+		return laposta.recibir();
 	}
 }
--- Proyecto-Taller/src/usuariosYAsistente/UsuarioGenerico.java
+++ Proyecto-Taller/src/usuariosYAsistente/UsuarioGenerico.java
@@ -4,13 +4,5 @@ import sockets.Cliente;
 
 public abstract class UsuarioGenerico {
 	public String nombre;
-	Cliente laposta;
 
-	public void enviarMensaje(String mensaje) {
-		laposta.enviar(nombre + ": " + mensaje);
-	}
-
-	public String recibirMensaje() {
-		return laposta.recibir();
-	}
 }
