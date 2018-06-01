10c61d0935c6b5a859ae83b2cf9a8eba3b077b8e
fedem <fedem@DESKTOP-AL24QAO>
2018-06-01 18:04:50 -0300

    agregadas las leyes

--- Proyecto-Taller/Asistente/resolvedores/Crear.java
+++ Proyecto-Taller/Asistente/resolvedores/Crear.java
@@ -11,20 +11,22 @@ public class Crear {
 		Agradecer agradecer = new Agradecer();
 		CalculoString calcular = new CalculoString();
 		ChuckNorris chuck = new ChuckNorris();
+		LeyesRobotica leyes = new LeyesRobotica();
 		Despedida despedida = new Despedida();
 		Fecha fecha = new Fecha();
 		Simpsons simpsons = new Simpsons();
 		Tiempo tiempo = new Tiempo();
 		Unidades_S_Metrico unidades = new Unidades_S_Metrico();
-		
+
 		agradecer.siguiente(fecha);
 		fecha.siguiente(tiempo);
-		tiempo.siguiente(simpsons);
+		tiempo.siguiente(leyes);
+		leyes.siguiente(simpsons);
 		simpsons.siguiente(chuck);
 		chuck.siguiente(calcular);
 		calcular.siguiente(unidades);
 		unidades.siguiente(despedida);
-		
+
 		return agradecer;
 	}
 
