8d9b4e989d50b62b55f773b9fd03f3aba9aadf42
fedemarkoo <fede.markoo@gmail.com>
2018-06-20 11:04:22 -0300

    alguien corrija los cochinos gifs!

--- Proyecto-Taller/Asistente/resolvedores/Giphy.java
+++ Proyecto-Taller/Asistente/resolvedores/Giphy.java
@@ -2,16 +2,18 @@ package resolvedores;
 
 import java.io.*;
 import java.net.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.nio.channels.*;
+import java.util.regex.*;
 
 public class Giphy extends RespuestaGenerico {
 
 	public String intentarResponder(String mensaje) {
 		if (consulta(mensaje)) {
 			Matcher tema = Pattern.compile("de (.*)").matcher(mensaje);
-			if (tema.find())
+			if (tema.find()) {
+				
 				return obtenerTodo(tema.group(1).replace(" ", "+"));
+			}
 		}
 		return null;
 	}
@@ -33,12 +35,24 @@ public class Giphy extends RespuestaGenerico {
 				if (linea.contains("img")) {
 					String link = linea.substring(26, linea.lastIndexOf("><") - 2);
 					String htmlImagen = linea.substring(linea.indexOf("<im"), linea.indexOf("/>") + 2);
+					try {
+						saveImage(htmlImagen.substring(10,htmlImagen.length()-3), "C:\\Users\\fedem\\Escritorio\\giphy.gif");
+					} catch (IOException e) {
+					}
 					return link + "\n" + htmlImagen;
 				}
 		} catch (Exception e) {
 			System.out.println(e.getLocalizedMessage());
 		}
 		return null;
+	}
+
+	public void saveImage(String imageUrl, String destinationFile) throws IOException {
+		URL website = new URL(imageUrl);
+		ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+		FileOutputStream fos = new FileOutputStream(destinationFile);
+		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		fos.close();
 	}
 
 }
