dd7818db34c64aa3cee1df7032e60b9e96359ed6
Pokengineer <vfacuu@gmail.com>
2018-06-22 19:46:29 -0300

    youtube en aplicacion y en jpanel

--- Proyecto-Taller/Ventanas/vistas/YouTube.java
+++ Proyecto-Taller/Ventanas/vistas/YouTube.java
@@ -1,31 +1,24 @@
 package vistas;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.net.URL;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
-import javax.media.Manager;
-import javax.media.Player;
-import javax.swing.JFrame;
 
-public class YouTube extends JFrame {
-	private static final long serialVersionUID = 1L;
-
-	public YouTube() {
-		setLayout(new BorderLayout());
-		try {
-			// file you want to play
-			URL mediaURL = new URL("https://www.youtube.com/watch?v=h4yVLn_djsE"); // Whatever
-			// create the media player with the media url
-			Player mediaPlayer;
-			mediaPlayer = Manager.createRealizedPlayer(mediaURL);
-			// get components for video and playback controls
-			Component video = mediaPlayer.getVisualComponent();
-			Component controls = mediaPlayer.getControlPanelComponent();
-			add(video, BorderLayout.CENTER);
-			add(controls, BorderLayout.SOUTH);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+public class YouTube extends Application {
+	public static void main(String[] args) {
+		launch(args);
 	}
+
+	@Override
+	public void start(Stage stage) {
+		WebView webview = new WebView();
+		webview.getEngine().load("https://www.youtube.com/embed/yOL-EJZjmp0?autoplay=1");
+		webview.setPrefSize(640, 390);
+		stage.setScene(new Scene(webview));
+		stage.show();
+	}
+	
+	
 }
---
+++ Proyecto-Taller/Ventanas/vistas/Youtube2.java
@@ -0,0 +1,61 @@
+package vistas;
+import javax.swing.*;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+public class Youtube2 {
+
+    private static void initAndShowGUI() {
+        // This method is invoked on the EDT thread
+        JFrame frame = new JFrame("Swing and JavaFX");
+        final JFXPanel fxPanel = new JFXPanel();
+        frame.add(fxPanel);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                initFX(fxPanel);
+            }
+       });
+    }
+
+    private static void initFX(JFXPanel fxPanel) {
+        // This method is invoked on the JavaFX thread
+        Scene scene = createScene();
+        fxPanel.setScene(scene);
+    }
+
+    private static Scene createScene() {    
+		WebView webview = new WebView();
+		webview.getEngine().load("https://www.youtube.com/embed/yOL-EJZjmp0?autoplay=1");
+		webview.setPrefSize(640, 390);
+		Scene  scene  =  new  Scene(webview);
+        return (scene);
+    }
+    
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                initAndShowGUI();
+            }
+        });
+    }
+}
